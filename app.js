6b3d7402a0d7d280a8cd1c2b9c66bce3478c1635
Patrick o'Shea <oshea_patrick@hotmail.com>
2023-04-02 14:42:11 -0400

    more fun

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config({path: __dirname + '/.env'})
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT || 3000
+const token = process.env.BOT_TOKEN
+const eventsApi = require('@slack/events-api')
+const slackEvents = eventsApi.createEventAdapter(process.env.SIGNING_SECRET)
+const { WebClient, LogLevel } = require("@slack/web-api");
+const client = new WebClient(token, {
+    logLevel: LogLevel.DEBUG
+});
+app.use('/', slackEvents.expressMiddleware())
+slackEvents.on("message", async(event) => {
+    console.log(event)
+})
+//YOUR CODE HERE
+app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`)
+})
